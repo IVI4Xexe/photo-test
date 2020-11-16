d06208011aa4deb2845a87c6c25686a610cd73bd
Max <max.meier.13@live.de>
2020-11-16 15:06:54 +0100

    testing with google maps url

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # photo-test
 
 add a folder named "screenshots" !
+
+
+execute with:
+
+node app.js [options]
+    --lat=[float]: latitude
+    --long=[float]: longitude
+    --debug=[bool: [true|false]]: show mouse in browser
+    --mode=[string: [maps]]: use google maps
+
+
+example: 
+    node app.js --lat=48.7451191 --long=9.3222745  --debug=true --mode=maps
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const puppeteer = require('puppeteer');
 const {installMouseHelper} = require('./install-mouse-helper');
-const helper = require('./maps-helper');
-
+const maps = require('./maps');
+const argv = require('minimist')(process.argv.slice(2));
 
 (async () => {
-    // var myArgs = process.argv.slice(2);
-    // console.log('myArgs: ', myArgs);
+    //console.log(argv);
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     page.setViewport({
@@ -14,55 +13,19 @@ const helper = require('./maps-helper');
         deviceScaleFactor: 1,
     })
 
-    const x = page.viewport().width / 2;
-    const y = page.viewport().height / 2;
-    const dist = 50;
+    //debug Mode
+    if(argv.debug)
+        await installMouseHelper(page);  
 
-    //only for debugging!
-    await installMouseHelper(page);
-
-
-    for(var rot = 0; rot < 4; rot++){
-        await page.goto('https://www.google.de/maps/@48.7441421,9.3224667,115a,35y,45.88t/data=!3m1!1e3?hl=de', {"waitUntil" : "networkidle0"});
-
-        await helper.consentToCoockies(page);
-        await helper.switchTo3D(page);
-        await helper.tiltView(page);
-        await helper.rotate(page, rot);
-        await helper.removeLabels(page);
-        await helper.removeIcons(page);
-    
-        //wait to load page
-        await page.waitForTimeout(5000);
-    
-        for(var i = 0; i < 10; i++){
-            var distMultiplier = Math.floor(i / 2) + 1;
-            for(var j = 0; j<distMultiplier; j++){
-                console.log(rot,i,j);
-                var xMove = x;
-                var yMove = y;
-                
-                await page.screenshot({path: `screenshots/screenshot${rot}-${i}-${j}.png`});
-    
-                if(i % 4 == 0){
-                    xMove += dist;
-                }else if(i % 4 == 1){
-                    yMove += dist;
-                }else if(i % 4 == 2){
-                    xMove -= dist;
-                }else if(i % 4 == 3){
-                    yMove -= dist;
-                }
-        
-                await page.mouse.down();
-                await page.mouse.move(x, y);
-                await page.waitForTimeout(100);
-                await page.mouse.move(xMove, yMove);
-                await page.mouse.up();
-            }
-        }
+    //Mode
+    switch (argv.mode) {
+        case 'maps':
+            await maps.execute(page, argv);
+            break;
+        default:
+            break;
     }
-  
+    
     await page.waitForTimeout(5000);
 
     await browser.close();
