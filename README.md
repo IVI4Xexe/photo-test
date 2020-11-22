d9768378fb87a48ca9f0afbc2cfb5e261218c4ae
Max <max.meier.13@live.de>
2020-11-22 13:40:43 +0100

    executing 4 browsers in parallel

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ add a folder named "screenshots" !
 * `--lat=[float]` latitude
 * `--long=[float]` longitude
 * `--debug=[bool: [true|false]]` show mouse in browser
+* `--delay=[int]` seconds waiting to load google maps, default: 1
 
 
 ## example: 
-`node app.js --lat=48.7451191 --long=9.3222745  --debug=true`
+`node app.js --lat=48.7451191 --long=9.3222745  --delay=10`
--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const puppeteer = require('puppeteer');
 const {installMouseHelper} = require('./install-mouse-helper');
-const helper = require('./maps-helper');
+const helper = require('./helper');
 const argv = require('minimist')(process.argv.slice(2));
 
 (async () => {
+    const delay = argv.delay != null ? parseInt(argv.delay) : 1;
+    const rots = [0,1,2,3];
+    const promises = rots.map(rot => excecuteAsync(rot, delay));
+
+    await Promise.all(promises);    
+})();
+
+async function excecuteAsync(rot, delay){
+    console.log(`promise ${rot} started`)
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     page.setViewport({
@@ -13,60 +22,57 @@ const argv = require('minimist')(process.argv.slice(2));
     })
 
     //debug Mode
-    console.log(argv.debug)
     if(argv.debug == "true")
-        await installMouseHelper(page);  
+        await installMouseHelper(page);
 
     const x = page.viewport().width / 2;
     const y = page.viewport().height / 2;
     const dist = 50;
 
+    //const url = `https://www.google.de/maps/@${argv.lat},${argv.long},115a,35y,45.88t/data=!3m1!1e3?hl=de`;
+    const url = `https://www.google.de/maps/@${argv.lat},${argv.long},${20}z/data=!3m1!1e3?hl=de`;
+    await page.goto(url, {"waitUntil" : "networkidle0"});
 
-    for(var rot = 0; rot < 4; rot++){
-        //const url = `https://www.google.de/maps/@${argv.lat},${argv.long},115a,35y,45.88t/data=!3m1!1e3?hl=de`;
-        const url = `https://www.google.de/maps/@${argv.lat},${argv.long},${20}z/data=!3m1!1e3?hl=de`;
-        console.log(url)
-        await page.goto(url, {"waitUntil" : "networkidle0"});
+    await helper.consentToCoockies(page);
+    await helper.switchTo3D(page);
+    await helper.tiltView(page);
+    await helper.rotate(page, rot);
+    await helper.removeLabels(page);
+    await helper.removeIcons(page);
 
-        await helper.consentToCoockies(page);
-        await helper.switchTo3D(page);
-        await helper.tiltView(page);
-        await helper.rotate(page, rot);
-        await helper.removeLabels(page);
-        await helper.removeIcons(page);
-    
-        //wait to load page
-        await page.waitForTimeout(5000);
+    //wait to load page
+    await page.waitForTimeout(1000 * delay);
 
-        for(var i = 0; i < 10; i++){
-            var distMultiplier = Math.floor(i / 2) + 1;
-            for(var j = 0; j<distMultiplier; j++){
-                
-                var xMove = x;
-                var yMove = y;
-                
-                await page.screenshot({path: `screenshots/screenshot${rot}-${i}-${j}.png`});
-    
-                if(i % 4 == 0){
-                    xMove += dist;
-                }else if(i % 4 == 1){
-                    yMove += dist;
-                }else if(i % 4 == 2){
-                    xMove -= dist;
-                }else if(i % 4 == 3){
-                    yMove -= dist;
-                }
-        
-                await page.mouse.down();
-                await page.mouse.move(x, y);
-                await page.waitForTimeout(100);
-                await page.mouse.move(xMove, yMove);
-                await page.mouse.up();
+    for(var i = 0; i < 10; i++){
+        var distMultiplier = Math.floor(i / 2) + 1;
+        for(var j = 0; j<distMultiplier; j++){
+            
+            var xMove = x;
+            var yMove = y;
+            
+            await page.screenshot({path: `screenshots/screenshot${rot}-${i}-${j}.png`});
+
+            if(i % 4 == 0){
+                xMove += dist;
+            }else if(i % 4 == 1){
+                yMove += dist;
+            }else if(i % 4 == 2){
+                xMove -= dist;
+            }else if(i % 4 == 3){
+                yMove -= dist;
             }
+    
+            await page.mouse.down();
+            await page.mouse.move(x, y);
+            await page.waitForTimeout(100);
+            await page.mouse.move(xMove, yMove);
+            await page.mouse.up();
         }
     }
-    
-    await page.waitForTimeout(5000);
 
     await browser.close();
-})();
+
+    console.log(`promise ${rot} returns`)
+
+    return;
+};
